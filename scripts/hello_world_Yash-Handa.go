ad72893edcb883c4e8f257050bbf6ff40f5ad510
Yash Handa <yashhanda7@yahoo.com>
2020-10-17 14:27:44 +0530

    Create hello_world_Yash-Handa.go

---
+++ scripts/hello_world_Yash-Handa.go
@@ -0,0 +1,11 @@
+// LANGUAGE: Golang
+// ENV: go v1.15.0
+// AUTHOR: Yash-Handa
+// GITHUB: https://github.com/Yash-Handa/
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello World")
+}
