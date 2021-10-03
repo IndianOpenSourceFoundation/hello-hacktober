2b479c912c25f37677a793de95862467aea1d645
Aarti Kushwaha <77312640+AartiKushwaha@users.noreply.github.com>
2021-10-03 12:39:19 +0530

    Update hello_world_AartiKushwaha.cpp

--- scripts/hello_world_AartiKushwaha.cpp
+++ scripts/hello_world_AartiKushwaha.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
+// LANGUAGE: C++
+// ENV: MINGW
+// AUTHOR: Aarti Kushwaha
+// GITHUB: https://github.com/AartiKushwaha
+// Date: 03/10/2021
+
+#include<iostream>     //Header library
 using namespace std;
 
 int main(){
-    cout<<"Hello World"<<endl;
+    cout<<"Hello World"<<endl;     //to print on the console
     return 0;
 }
+
+//End of program
