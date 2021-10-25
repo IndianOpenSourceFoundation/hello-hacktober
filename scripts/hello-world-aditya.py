e136bed8a7132649841d7d60a2abc3c473c975a5
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2021-10-24 22:41:31 -0300

    feat: log message

--- scripts/hello-world-aditya.py
+++ scripts/hello-world-aditya.py
@@ -1,5 +1,5 @@
 # LANGUAGE: Python
-# ENV: Python 3.9.1
+# ENV: Python 3.8.6rc1
 # AUTHOR: Aditya
-# GITHUB: https://github.com/yupitsadi
+# GITHUB: https://github.com/Adi7171
 print("Hello World!")
---
+++ scripts/hello_world_guilhermeorcezi.js
@@ -0,0 +1,8 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: guilhermeorcezi
+// GITHUB: https://github.com/guilhermeorcezi
+
+
+const message = "HELLO WORLD! o/";
+console.log(message);
